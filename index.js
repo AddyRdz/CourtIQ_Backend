be4f376bfce9990d46a0c61d9bebea8146edda78
Addy Rodriguez <rdz.33.a@gmail.com>
2024-10-25 14:32:58 -0500

    added middleware

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import morgan from "morgan";
 import playersRouter from "./routes/players.js";
 import teamsRouter from "./routes/teams.js";
 
 dotenv.config();
-
-// connect to the database
-mongoose.connect(process.env.MONGODB_URI);
-
 const app = express();
 const PORT = 4000;
+
+// Connect to the database
+mongoose.connect(process.env.MONGODB_URI);
+
 
 function logger(req, res, next) {
   console.log(`Received a request: ${req.method}, ${req.url}, ${req.hostname}`);
@@ -19,14 +20,25 @@ function logger(req, res, next) {
 
 app.use(logger);
 
+// Middleware
+app._router.use(morgan('dev'));
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
+// Routes
 app.get("/", (req, res) => {
   res.send("HOME");
 });
 
+
 app.use("/players", playersRouter);
 app.use("/teams", teamsRouter);
+
+// Error Middleware
+app.use((e, req, res, next) => {
+  console.error(e);
+  res.status(500).json({extended:true})
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
